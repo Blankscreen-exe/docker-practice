c1dd817eebc16487b164ec152edf8e799d81fa78
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2023-10-13 19:48:02 +0000

    log data

---
+++ 004. generate a log file outside of the container/generate_logs.py
@@ -0,0 +1,33 @@
+# This module is used for generating dummy logs along with timestamps
+# These logs are generated to the stdout
+
+import time
+import random
+
+# dummy log messages
+log_messages = [
+    "Application started",
+    "Processing request...",
+    "Warning: Disk space is low",
+    "Error: Connection timeout",
+    "Database updated successfully",
+]
+
+def generate_dummy_logs():
+    """throws log messages to stdout infinitly along with timestamps
+    """
+    while True:
+        # Choose a random log message
+        log_message = random.choice(log_messages)
+        
+        # Get the current timestamp
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+        
+        # Print the log message with timestamp
+        print(f"[{timestamp}] - {log_message}")
+        
+        # Sleep for a random interval (simulating log generation)
+        time.sleep(random.uniform(1, 5))
+
+if __name__ == "__main__":
+    generate_dummy_logs()
